126c845e2a0c4ae7af28b165b232ee6322cfaa7c
GoldenStack <goldenfire64yt@gmail.com>
2023-08-16 00:19:32 -0500

    Revert test change and fix inventory change issue

--- src/main/java/net/minestom/server/entity/Player.java
+++ src/main/java/net/minestom/server/entity/Player.java
@@ -1727,7 +1727,9 @@ public class Player extends LivingEntity implements CommandSender, Localizable, 
         var closedInventory = getOpenInventory();
         if (closedInventory != null) {
             if (closedInventory.removeViewer(this)) {
-                this.openInventory = null;
+                if (closedInventory == getOpenInventory()) {
+                    this.openInventory = null;
+                }
                 return true;
             }
         } else {
